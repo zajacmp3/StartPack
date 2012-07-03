c11f2519d94e4970b661cc7a1bbc9cf3b921db25
Mateusz Zając <deejay.zajac@gmail.com>
2012-07-03 12:38:50 +0200

    First working properly version

--- src/com/zajacmp3/startpack/StartPack.java
+++ src/com/zajacmp3/startpack/StartPack.java
@@ -1,7 +1,6 @@
 package com.zajacmp3.startpack;
 
 import java.io.File;
-
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
@@ -15,10 +14,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class StartPack extends JavaPlugin implements Listener{
-	static ItemStack helmet;
-	static ItemStack armor;
-	static ItemStack legs;
-	static ItemStack boots;
+	static int helmet;
+	static int armor;
+	static int legs;
+	static int boots;
+	static int additionalItems;
 	public void onDisable(){
 		PluginDescriptionFile description = this.getDescription();
 		System.out.println("StartPack" + description.getVersion()+" is stopping...");
@@ -42,10 +42,11 @@ public class StartPack extends JavaPlugin implements Listener{
 		}
 		else{
 			FileConfiguration config = this.getConfig();
-			helmet = config.getItemStack("Configuration.Helmet");
-			armor = config.getItemStack("Configuration.Armor");
-			legs = config.getItemStack("Configuration.Legs");
-			boots = config.getItemStack("Configuration.Boots");
+			helmet = config.getInt("Configuration.Helmet");
+			armor = config.getInt("Configuration.Armor");
+			legs = config.getInt("Configuration.Legs");
+			boots = config.getInt("Configuration.Boots");
+			additionalItems = config.getInt("Configuration.AdditionalItems");
 		}
 		
 	}
@@ -56,6 +57,9 @@ public class StartPack extends JavaPlugin implements Listener{
 		config.addDefault("Configuration.Armor",299);
 		config.addDefault("Configuration.Legs",300);
 		config.addDefault("Configuration.Boots",301);
+		config.addDefault("Configuration.AdditionalItems",1);
+		config.addDefault("Configuration.AdditionalItem - 1.ID", 1);
+		config.addDefault("Configuration.AdditionalItem - 1.Amount", 1);
 		config.options().copyDefaults(true);
 		saveConfig();
 		load();
@@ -65,8 +69,14 @@ public class StartPack extends JavaPlugin implements Listener{
 		Player logging = event.getPlayer();
 		boolean exist = (new File("world/players/"+logging.getName()+".dat")).exists();
 		if(exist==false){
-			logging.getInventory().addItem(armor);
-			logging.getInventory().setChestplate(armor);
+			logging.getInventory().setChestplate(new ItemStack(armor, 1));
+			logging.getInventory().setHelmet(new ItemStack(helmet,1));
+			logging.getInventory().setBoots(new ItemStack(boots,1));
+			logging.getInventory().setLeggings(new ItemStack(legs,1));
+			for(int i=1;i<=additionalItems;i++){
+				FileConfiguration configuration = this.getConfig();
+				logging.getInventory().addItem(new ItemStack(configuration.getInt("Configuration.AdditionalItem - "+i+".ID"),configuration.getInt("Configuration.AdditionalItem - "+i+".Amount")));
+			}
 		}
 	}
 }
